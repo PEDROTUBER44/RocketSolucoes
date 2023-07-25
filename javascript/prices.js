12c8b51ab378f418739576e56251d0670640cb63
Pedro Rosendo <77652411+PEDROTUBER44@users.noreply.github.com>
2023-07-25 20:07:57 +0000

    Update de varias coisas

---
+++ javascript/prices.js
@@ -0,0 +1,112 @@
+// Botão de folha sulfite colorida
+var buttonsulfitecolor = document.getElementById("sulfitecolor");
+function changebuttonsulfitecolorText(buttonsulfitecolor) {
+    buttonsulfitecolor.innerHTML = "R$0,65";
+}
+function reversebuttonsulfitecolorText(buttonsulfitecolor) {
+    buttonsulfitecolor.innerHTML = "Colorido";
+}
+
+// Botão de folha sulfite preta e branca
+var buttonsulfitepeb = document.getElementById("sulfitepeb");
+function changebuttonsulfitepebText(buttonsulfitepeb) {
+    buttonsulfitepeb.innerHTML = "R$0,50";
+}
+function reversebuttonsulfitepebText(buttonsulfitepeb) {
+    buttonsulfitepeb.innerHTML = "Preto e Branco";
+}
+
+
+// Botão de Xerox Colorida
+var buttonxeroxcolor = document.getElementById("xeroxcolor");
+function changebuttonxeroxcolorText(buttonxeroxcolor) {
+    buttonxeroxcolor.innerHTML = "R$0,65";
+}
+function reversebuttonxeroxcolorText(buttonxeroxcolor) {
+    buttonxeroxcolor.innerHTML = "Colorida";
+}
+
+// Botão de Xerox Preto e Branco
+var buttonxeroxpeb = document.getElementById("xeroxpeb");
+function changebuttonxeroxpebText(buttonxeroxpeb) {
+    buttonxeroxpeb.innerHTML = "R$0,50";
+}
+function reversebuttonxeroxpebText(buttonxeroxpeb) {
+    buttonxeroxpeb.innerHTML = "Preto e Branco";
+}
+
+
+// Botão de Fotografia A4 Colorida
+var buttonphotographica4color = document.getElementById("photographica4color");
+function changebuttonphotographica4colorText(buttonphotographica4color) {
+    buttonphotographica4color.innerHTML = "R$6,00";
+}
+function reversebuttonphotographica4colorText(buttonphotographica4color) {
+    buttonphotographica4color.innerHTML = "Colorida";
+}
+
+// Botão de Fotografia A4 Preto e Branco
+var buttonphotographica4peb = document.getElementById("photographica4peb");
+function changebuttonphotographica4pebText(buttonphotographica4peb) {
+    buttonphotographica4peb.innerHTML = "R$5,00";
+}
+function reversebuttonphotographica4pebText(buttonphotographica4peb) {
+    buttonphotographica4peb.innerHTML = "Preto e Branco";
+}
+
+
+// Botão de Fotografia A5 Colorida
+var buttonphotographica5color = document.getElementById("photographica5color");
+function changebuttonphotographica5colorText(buttonphotographica5color) {
+    buttonphotographica5color.innerHTML = "R$3,00";
+}
+function reversebuttonphotographica5colorText(buttonphotographica5color) {
+    buttonphotographica5color.innerHTML = "Colorida";
+}
+
+// Botão de Fotografia A5 Preto e Branco
+var buttonphotographica5peb = document.getElementById("photographica5peb");
+function changebuttonphotographica5pebText(buttonphotographica5peb) {
+    buttonphotographica5peb.innerHTML = "R$2,75";
+}
+function reversebuttonphotographica5pebText(buttonphotographica5peb) {
+    buttonphotographica5peb.innerHTML = "Preto e Branco";
+}
+
+
+// Botão de Fotografia 10x15 Colorida
+var buttonphotographica10x15color = document.getElementById("photographica10x15color");
+function changebuttonphotographica10x15colorText(buttonphotographica10x15color) {
+    buttonphotographica10x15color.innerHTML = "R$2,00";
+}
+function reversebuttonphotographica10x15colorText(buttonphotographica10x15color) {
+    buttonphotographica10x15color.innerHTML = "Colorida";
+}
+
+// Botão de Fotografia 10x15 Preto e Branco
+var buttonphotographica10x15peb = document.getElementById("photographica10x15peb");
+function changebuttonphotographica10x15pebText(buttonphotographica10x15peb) {
+    buttonphotographica10x15peb.innerHTML = "R$1,75";
+}
+function reversebuttonphotographica10x15pebText(buttonphotographica10x15peb) {
+    buttonphotographica10x15peb.innerHTML = "Preto e Branco";
+}
+
+
+// Botão de Convites sem arte
+var buttonartlessinvitations = document.getElementById("artlessinvitations");
+function changebuttonartlessinvitations(buttonartlessinvitations) {
+    buttonartlessinvitations.innerHTML = "R$2,00";
+}
+function reversebuttonartlessinvitations(buttonartlessinvitations) {
+    buttonartlessinvitations.innerHTML = "S/Arte Inclusa";
+}
+
+// Botão de Convites com arte
+var buttoninvitationswithart = document.getElementById("invitationswithart");
+function changebuttoninvitationswithart(buttoninvitationswithart) {
+    buttoninvitationswithart.innerHTML = "R$4,00";
+}
+function reversebuttoninvitationswithart(buttoninvitationswithart) {
+    buttoninvitationswithart.innerHTML = "C/Arte Inclusa";
+}
